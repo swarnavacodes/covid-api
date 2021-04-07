c202172fef8b6e91b638efdfe63e59a2f5fd6b29
Swarnava Ganguly <swarnava.ganguly@gmail.com>
2021-04-07 12:01:45 -0500

    Added exception handeling for redis

--- src/main/java/com/sg/covidapi/config/RedisConfig.java
+++ src/main/java/com/sg/covidapi/config/RedisConfig.java
@@ -1,7 +1,11 @@
 package com.sg.covidapi.config;
 
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -11,14 +15,47 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
-public class RedisConfig {
+public class RedisConfig extends CachingConfigurerSupport {
 
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object o) {
+
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object o, Object o1) {
+
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object o) {
+
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+
+            }
+        };
+    }
     // This template is just as a reference, only used when we need to explicitly store data in redis and redis is used as a DB
     @Bean
     public JedisConnectionFactory connectionFactory() {
+
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName("localhost");
-        configuration.setPort(6379);
+        try{
+
+            configuration.setHostName("localhost");
+            configuration.setPort(6379);
+
+        }
+        catch (RedisConnectionFailureException e)
+        {
+            e.getMessage();
+        }
         return new JedisConnectionFactory(configuration);
     }
 
